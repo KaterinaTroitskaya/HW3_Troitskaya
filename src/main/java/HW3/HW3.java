3cbaf54d9a9096409134dcd36e2c541e987dcef4
kate <2951525@gmail.com>
2022-02-10 00:56:56 +0300

    Commit 2: Задание 2 и 3

--- src/main/java/HW3/HW3.java
+++ src/main/java/HW3/HW3.java
@@ -2,12 +2,20 @@ package HW3;
 
 import java.util.Arrays;
 
+import static java.lang.System.out;
+
 public class HW3 {
     public static void main(String[] args) {
         int [] arr = {1,1,0,0,1,0,1,1,0,0};
         System.out.println(Arrays.toString(arr));
         int [] newarr = invertTheArray(arr);
         System.out.println(Arrays.toString(newarr));
+        int [] hugearr = new int[100];
+        fillTheArray(hugearr);
+        System.out.println(Arrays.toString(hugearr));
+        int [] arrForTask3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        smallNumbersDoubler(arrForTask3);
+        out.println(Arrays.toString(arrForTask3));
 
     }
     //region Task1
@@ -23,4 +31,22 @@ public class HW3 {
         return arr;
     }
     //endregion
+    //region Task2
+    public static void fillTheArray(int[]arr){
+        for (int i = 0, j=1; i < arr.length; i++, j++){
+            arr[i]=j;
+        }
+    }
+    //endregion
+    //region Task3
+    public static void smallNumbersDoubler(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            if (arr[i]<6){
+                arr[i] = arr[i]*2;
+            }
+        }
+    }
+    //endregion
+
+
 }
